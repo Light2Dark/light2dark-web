5727a11f1d49f43e793c42dc178b03cb6920893b
Shahmir Varqha <Sham9871@gmail.com>
2022-01-01 02:19:17 +0800

    added three assets

--- src/components/Main/Three.tsx
+++ src/components/Main/Three.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 import React, {useRef, useState} from 'react'
 import {Canvas, useFrame} from '@react-three/fiber'
 
+import {useGLTF, useTexture} from "@react-three/drei"
+
 interface Props {
     className?: string
     colors?: string
@@ -38,17 +40,33 @@ function Rose(props: JSX.IntrinsicElements["mesh"]) {
     )
 }
 
+const smokeImg = "../../assets/models/smoke-1.png"
+
+function Rain(props: JSX.IntrinsicElements["mesh"]) {
+    // const smokeMap = useTexture("../../assets/models/smoke-1.png");
+    // const textureProps = useTexture({
+    //     smoke: "smoke-1.png",
+    // });
+    
+
+    return(
+        <mesh>
+            <sphereGeometry args={[1,32,32]} />
+            <meshStandardMaterial color={"blue"} />
+            {/* <meshLambertMaterial map={textureProps.smoke} transparent={true} /> */}
+        </mesh>
+    )
+}
+
 const Three = (): JSX.Element => {
     return (
         <>
             {/* <h2 className = {className}>Three.js video here, rain falling on white roses?</h2> */}
 
             <CanvasStyled>
-                <ambientLight intensity={0.1} />
-                {/* <pointLight position={[10,10,10]} />
-                <Box position={[0,0,0]} /> */}
-                <directionalLight color="red" position={[0,0,5]} />
-                <Rose />
+                <ambientLight intensity={1} color={0x55555} />
+                <directionalLight color="0xffeedd" position={[0,0,1]} />
+                <Rain />
             </CanvasStyled>
         </>
     )
@@ -57,6 +75,7 @@ const Three = (): JSX.Element => {
 const CanvasStyled = styled(Canvas)`
     height: 200px;
     color: ${props => props.theme.colors.darkBlue};
+    background-color: 0xffeedd;
     background-color: white;
     text-align: center;
 `
