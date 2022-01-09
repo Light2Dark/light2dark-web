d0fec65af46ed61a97549deaf402f31abfa8abc5
Shahmir Varqha <Sham9871@gmail.com>
2022-01-09 22:45:28 +0800

    some changes, pic is broken though

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components"
 import { SocialIcon } from "react-social-icons"
 
-type IconProps = {
-    url: string;
-    bgColor?: string;
-    fgColor?: string;
-    className?: string;
-}
+// type IconProps = {
+//     url: string;
+//     bgColor?: string;
+//     fgColor?: string;
+//     className?: string;
+// }
 
 const StyledDiv = styled.div`
     background-color: ${props => props.theme.colors.darkBlue};
@@ -19,41 +19,49 @@ const StyledDiv = styled.div`
     }
 `
 
-const Icon = ({url, bgColor, fgColor, className}: IconProps) => {
-    return(
-        <>
-            <SocialIcon url = {url} bgColor={bgColor} fgColor={fgColor} />
-        </>
-    )
-}
+// const Icon = ({url, bgColor, fgColor, className}: IconProps) => {
+//     return(
+//         <>
+//             <SocialIcon url = {url} bgColor={bgColor} fgColor={fgColor} />
+//         </>
+//     )
+// }
 
-const IconStyled = styled(Icon)<IconProps>`
-    border: 2px solid black;
-    color: #b6b6b6;
+// const IconStyled = styled(Icon)<IconProps>`
+//     border: 2px solid black;
+//     color: #b6b6b6;
     
+//     &:hover {
+//         border: 2px solid black;
+//     }
+// `
+
+const DivItem = styled.div`
+    transition: all 0.3s ease;
+
     &:hover {
-        border: 2px solid black;
+        transform: scale(1.1)
     }
 `
 
 const Footer = () => {
     return (
         <StyledDiv>
-            <div>
+            <DivItem>
                 <SocialIcon url="https://github.com/Light2Dark" bgColor="black" fgColor="#b6b6b6"/>
-            </div>
+            </DivItem>
 
-            <div>
+            <DivItem>
                 <SocialIcon url="https://www.instagram.com/shahms.v/"/>
-            </div>
+            </DivItem>
 
-            <div>
+            <DivItem>
                 <SocialIcon url="https://open.spotify.com/user/12148851158?si=55f59fd923724d66"/>
-            </div>
+            </DivItem>
 
-            <div>
+            <DivItem>
                 <SocialIcon url="https://www.linkedin.com/in/shahmir-varqha" />
-            </div>
+            </DivItem>
 
         </StyledDiv>
     )
